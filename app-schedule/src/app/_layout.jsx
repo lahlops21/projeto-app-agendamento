4bd149bf8a95d3854ff75cf66769cb858a7be1a5
Lais Lopes <laisllopes21@gmail.com>
2026-04-29 10:10:27 -0300

    usando drawer

--- app-schedule/src/app/_layout.jsx
+++ app-schedule/src/app/_layout.jsx
@@ -1,5 +1,21 @@
 import { Drawer } from "expo-router/drawer";
 
 export default function RootLayout() {
-  return <Drawer />;
+  return (<Drawer 
+    // define o estilo do drawer, objeto complexo
+    screenOptions={{
+      drawerStyle:{
+        backgroundColor: "#B6B7B9"
+      },
+      drawerLabelStyle: "#000"
+    }} >
+ 
+
+  <Drawer.Screen
+  name = "LoginScreen"
+  options={{
+    headerShown: false
+  }} />
+   </Drawer>
+  );
 }

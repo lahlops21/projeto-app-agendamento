b9463317b635482322811e380f4f0f9a6a8f5a09
Lais Lopes <laisllopess21@gmail.com>
2026-06-30 12:48:59 -0300

    refinando authContext

--- app-schedule/src/app/context/authContext.js
+++ app-schedule/src/app/context/authContext.js
@@ -1,20 +1,33 @@
 // Esse rquivo será responsável por planejar todas as requisições que o front-end irá fazer para a api
 
+import { createContext, useState } from "react";
+
 
 // async/await
 export const API_URL = 'http://localhost:8080';
 
 let tokenJwt = null;
-const controller = new AbortController();// cancela a request se exceder o tempo limite
+
+
+const TIMEOUT_MS = 10_000; // 10 segundos de espera depois da requisição 
+
+//Função para pegar o tokenJWT para ser usado nas próximas requisições 
+export function setToken(token){
+tokenJwt = token
+}
+
+// Função que limpa o token
+export function clearToken(){
+   tokenJwt = null 
+}
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }){
+    const[usuario, setUsuario] = useState(null)
+}
 
 export default async function request({method, path, body}) {
-
-
-    const api = {
-        get: (path) => request(path, 'GET'),
-        post: (path, body) => request(path, 'POST', body),
-        put: (path, body) => request(path, 'PUT', body),
-        delete: (path) => request(path, 'DELETE'),
-    }
-    return api;
+const controller = new AbortController();// cancela a request se exceder o tempo limite
+const timer = setTimeout();
 }
